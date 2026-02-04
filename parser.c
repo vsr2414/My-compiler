de8aa7fc07fd063cb172625b90985a8d3b9880ee
vsr2414 <visitha2414@gmail.com>
2026-02-04 21:26:57 +0530

    modified:   parser.c

--- parser.c
+++ parser.c
@@ -38,3 +38,38 @@ int symbolCount = 0;
 
 FILE *source;
 Token currentToken;
+
+/* ---------- FUNCTION DECLARATIONS ---------- */
+
+Token getNextToken();
+void advance();
+void error(const char *msg);
+
+void parseProgram();
+void parseStatement();
+void parseDeclaration();
+void parsePrint();
+int parseExpression();
+int parseTerm();
+
+int findSymbol(const char *name);
+void addSymbol(const char *name, int value);
+
+/* ---------- SYMBOL TABLE FUNCTIONS ---------- */
+
+int findSymbol(const char *name) {
+    for (int i = 0; i < symbolCount; i++) {
+        if (strcmp(table[i].name, name) == 0)
+            return i;
+    }
+    return -1;
+}
+
+void addSymbol(const char *name, int value) {
+    if (findSymbol(name) != -1)
+        error("Variable already declared");
+
+    strcpy(table[symbolCount].name, name);
+    table[symbolCount].value = value;
+    symbolCount++;
+}
