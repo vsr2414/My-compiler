39fda0677fd5f9bfa5e930709ec56619105a94fc
vsr2414 <visitha2414@gmail.com>
2026-02-07 23:58:59 +0530

    modified:   parser.c

--- parser.c
+++ parser.c
@@ -299,3 +299,7 @@ We are excited to introduce ChatGPT to get users’ feedback and learn about its
 /*
 We trained this model using Reinforcement Learning from Human Feedback (RLHF), using the same methods as InstructGPT⁠, but with slight differences in the data collection setup. We trained an initial model using supervised fine-tuning: human AI trainers provided conversations in which they played both sides—the user and an AI assistant. We gave the trainers access to model-written suggestions to help them compose their responses. We mixed this new dialogue dataset with the InstructGPT dataset, which we transformed into a dialogue format.
 */
+
+/*
+To create a reward model for reinforcement learning, we needed to collect comparison data, which consisted of two or more model responses ranked by quality. To collect this data, we took conversations that AI trainers had with the chatbot. We randomly selected a model-written message, sampled several alternative completions, and had AI trainers rank them. Using these reward models, we can fine-tune the model using Proximal Policy Optimization⁠. We performed several iterations of this process.
+*/
